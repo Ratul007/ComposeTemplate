4797c5bf4ac40afee41a11b947530a0b184bbbe4
Ratul007 <ratulmondal1@gmail.com>
2023-05-21 02:12:01 +0530

    4

--- app/src/main/java/com/example/compose/MainActivity4.kt
+++ app/src/main/java/com/example/compose/MainActivity4.kt
@@ -1,16 +1,33 @@
 package com.example.compose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.ui.theme.ComposeTheme
+import com.example.compose.ui.theme.Purple700
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity4 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +39,123 @@ class MainActivity4 : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+data class ProfileModel(
+    var age: String,
+    var name: String,
+    var email: String,
+)
+
+data class UserModel(
+    var profile: ProfileModel
+)
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Purple700,
+                title = {
+                    Text(
+                        text = "Simple API Request",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val id = remember {
+                    mutableStateOf(TextFieldValue())
+                }
+
+                val profile = remember {
+                    mutableStateOf(ProfileModel(
+                        age = "",
+                        name = "",
+                        email = ""
+                    ))
+                }
+
+                Text(
+                    text="API Sample",
+                    style= TextStyle(
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily.Cursive
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                TextField(
+                    label = { Text(text = "User ID")},
+                    value = id.value,
+                    onValueChange = { id.value = it }
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                    Button(
+                        onClick = {
+                            val data = sendRequest(
+                                id = id.value.text,
+                                profileState = profile
+                            )
+
+                            Log.d("Main Activity", profile.toString())
+                        }
+                    ) {
+                        Text(text = "Get Data")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(text = profile.component1().toString(), fontSize = 40.sp)
+            }
+        }
+    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
-    }
+
+fun sendRequest(
+    id: String,
+    profileState: MutableState<ProfileModel>
+) {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://192.168.43.232:3000/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api = retrofit.create(UserApiSea::class.java)
+
+    val call: Call<UserModel?>? = api.getUserById(id);
+
+    call!!.enqueue(object: Callback<UserModel?> {
+        override fun onResponse(call: Call<UserModel?>, response: Response<UserModel?>) {
+            if(response.isSuccessful) {
+                Log.d("Main", "success!" + response.body().toString())
+                profileState.value = response.body()!!.profile
+            }
+        }
+
+        override fun onFailure(call: Call<UserModel?>, t: Throwable) {
+            Log.e("Main", "Failed mate " + t.message.toString())
+        }
+    })
 }
